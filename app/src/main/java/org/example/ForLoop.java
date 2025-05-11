b0da8e78dc5d8207bbe8133ba4cc5179545cf4cc
Juunibyou <danijunn@gmail.com>
2025-05-11 17:05:45 -0500

    Stream API

--- app/src/main/java/org/example/ForLoop.java
+++ app/src/main/java/org/example/ForLoop.java
@@ -1,5 +1,37 @@
 package org.example;
 
-public class TraditionalLoop {
+public class ForLoop {
     
+    public int maximumUsingForLoop(int[] nums) {
+        int max = nums[0];
+        for (int num : nums){
+            if(num > max){
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    public int minimumUsingForLoop(int[] nums) {
+        int min = nums[0];
+        for (int num : nums){
+            if(num > min){
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    public int sumUsingForLoop(int[] nums) {
+        int sum = 0;
+        for (int num : nums){
+            sum += num;
+        }
+        return sum;
+    }
+
+    public int averageUsingForLoop(int[] nums) {
+        return sumUsingForLoop(nums)/nums.length;
+    }
 }
+
--- app/src/main/java/org/example/UsingStream.java
+++ app/src/main/java/org/example/UsingStream.java
@@ -1,5 +1,17 @@
 package org.example;
+import java.util.Arrays;
 
 public class UsingStream {
-    
+    public int maximumUsingStream(int[] nums) {
+        return Arrays.stream(nums).max().orElseThrow();
+    }
+    public int minimumUsingStream(int[] nums) {
+        return Arrays.stream(nums).min().orElseThrow();
+    }
+    public int sumUsingStream(int[] nums) {
+        return Arrays.stream(nums).sum();
+    }
+    public int averageUsingStream(int[] nums) {
+        return (int) Arrays.stream(nums).average().orElseThrow();
+    }
 }
